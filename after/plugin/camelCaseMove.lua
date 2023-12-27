a072a7fce45bb976768489e6f6d87317fa160200
VanaIgr <vanaigranov@gmail.com>
2023-12-27 17:37:34 -0600

    Change camel case motion keymaps

--- after/plugin/camelCaseMove.lua
+++ after/plugin/camelCaseMove.lua
@@ -1,12 +1,14 @@
 local m = require('mapping')
 
 m.n('<leader>w', '<Plug>CamelCaseMotion_w')
-m.n('<leader>b', '<Plug>CamelCaseMotion_b')
+m.n('<leader>b', 'h<Plug>CamelCaseMotion_b')
 m.n('<leader>e', '<Plug>CamelCaseMotion_e')
 m.n('<leader>ge', '<Plug>CamelCaseMotion_ge')
+
 m.x('<leader>w', '<Plug>CamelCaseMotion_iw')
-m.x('<leader>b', '<Plug>CamelCaseMotion_ib')
-m.x('<leader>e', '<Plug>CamelCaseMotion_ie')
-m.o('<leader>w', '<Plug>CamelCaseMotion_iw')
-m.o('<leader>b', '<Plug>CamelCaseMotion_ib')
-m.o('<leader>e', '<Plug>CamelCaseMotion_ie')
+m.x('<leader>b', 'h<Plug>CamelCaseMotion_b')
+m.x('<leader>e', '<Plug>CamelCaseMotion_e')
+
+m.o('<leader>w', '<Plug>CamelCaseMotion_ib')
+m.o('<leader>b', '<Plug>CamelCaseMotion_b')
+m.o('<leader>e', '<Plug>CamelCaseMotion_e')
