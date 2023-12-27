d38b461e581da071e491414791c54880ce8a383d
VanaIgr <vanaigranov@gmail.com>
2023-12-27 17:47:58 -0600

    Change plugins

--- lua/main/plugins.lua
+++ lua/main/plugins.lua
@@ -10,27 +10,32 @@ return packer.startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use{ 'rose-pine/neovim', as = 'rose-pine' } -- theme
-    use{ 'Wansmer/treesj', requires = { 'nvim-treesitter/nvim-treesitter' } } -- split/join
+    use{ 'Wansmer/treesj', requires = { 'VanaIgr/nvim-treesitter' } } -- split/join
+                                        -- ^ bugfix
     use{ 'mg979/vim-visual-multi' } -- multicursor
     use{ 'chentoast/marks.nvim' }
-    use{ 'ggandor/leap.nvim' } -- sneak
     use{ 'tpope/vim-fugitive' } -- git 
-    --use{ 'wellle/targets.vim' } -- replaced with local bugfix
+    use{ 'VanaIgr/targets.vim' } -- replaced with unmerged bugfix
     use{ 'kylechui/nvim-surround', tag = "*" }
     use{ 'mbbill/undotree' }
     use{ 'numToStr/Comment.nvim' }
-    use{ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use{ 'VanaIgr/nvim-treesitter', run = ':TSUpdate' } -- bugfix
     use{ 'bkad/CamelCaseMotion' }
     use{ 'petertriho/nvim-scrollbar' }
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    }
+    use{ 'nvim-lualine/lualine.nvim' }
+    use{ 'HiPhish/rainbow-delimiters.nvim' }
+    use{ 'echasnovski/mini.nvim' }
+
+    use{ 'ggandor/leap.nvim' } -- sneak
+    use{ 'VanaIgr/leap-by-word.nvim', requires = { 'ggandor/leap.nvim' } } -- unmerged bugfix
+    use{ 'ggandor/leap-ast.nvim', requires = { 'ggandor/leap.nvim' } }
+    use{ 'VanaIgr/nvim-easyword' }
 
     use{ 'hrsh7th/cmp-buffer' } -- autocomplete without lsp
     use{ 'hrsh7th/cmp-path' }
     use{ 'hrsh7th/cmp-cmdline' }
     use{ 'hrsh7th/cmp-nvim-lua' }
+
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.4',
@@ -46,7 +51,7 @@ return packer.startup(function(use)
             {'neovim/nvim-lspconfig'},
             {'hrsh7th/nvim-cmp'},
             {'hrsh7th/cmp-nvim-lsp'},
-            --{'L3MON4D3/LuaSnip'},
+            {'L3MON4D3/LuaSnip'},
         }
     }
 end)
