302d61c697e0d38cf24da5a13068763f52fb6189
VanaIgr <vanaigranov@gmail.com>
2023-12-27 11:13:38 -0600

    separate getting selected text from running the selection

---
+++ lua/main/getSelection.lua
@@ -0,0 +1,74 @@
+local vim = vim -- fix lsp warning
+local destr = table.unpack or unpack
+
+local M = {}
+
+function M.printError(msg)
+    vim.cmd.redraw() -- https://stackoverflow.com/a/19206860/18704284
+    vim.api.nvim_echo({{ msg, 'ErrorMsg' }}, true, {})
+end
+
+function M.fixMarkerCoords(sl, sc, el, ec) -- pray that this will work
+    sl = sl - 1
+    el = el - 1
+    sc = sc - 1
+    ec = ec - 1
+
+    local lastCharLen = vim.fn.len(vim.fn.strpart(vim.fn.getline(el+1), ec, 1, false))
+    ec = ec + lastCharLen
+
+    return { sl, sc, el, ec }
+end
+
+-- we need to do expr + return 'g@' instead of feedkeys because ?
+--run command with text object
+function M.callOpfunc(func, args)
+    local cursorPos = vim.fn.getpos('.')
+    _G.OpFunc = function(type)
+        vim.fn.setpos('.', cursorPos)
+
+        if type ~= 'line' and type ~= 'char' then M.printError('Opfunc type '..type..' is not supported'); return end
+        local sl, sc = destr(vim.fn.getpos("'["), 2, 3)
+        local el, ec = destr(vim.fn.getpos("']"), 2, 3)
+        if type == 'line' then
+            sc = 1
+            ec = 2147483647 -- ec = -1 counts backwards
+        end -- selecting 'ap' paragraph text object at the end of file
+            -- includes only the first character of the last line.
+
+        func(args, M.fixMarkerCoords(sl, sc, el, ec))
+    end
+    vim.cmd[=[set opfunc=v:lua.OpFunc]=]
+    return 'g@'
+end
+
+--run command with visual selection
+function M.callVisual(func, args)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'nx', true)
+    local type = vim.fn.visualmode()
+    if type ~= 'v' and type ~= 'V' then M.printError('Visual mode '..type..' is not supported'); return end -- visual block
+
+    local sl, sc = destr(vim.fn.getpos("'<"), 2, 3)
+    local el, ec = destr(vim.fn.getpos("'>"), 2, 3)
+
+    if vim.api.nvim_get_option('selection') == 'exclusive' then
+        sl = sl - 1
+        sc = sc - 1
+        el = el - 1
+        ec = ec - 1 -- end is already exclusive
+    else
+        sl, sc, el, ec = destr(M.fixMarkerCoords(sl, sc, el, ec))
+    end
+
+    func(args, { sl, sc, el, ec })
+end
+
+--run command with current line
+function M.callLine(func, args)
+    local l = vim.fn.getpos('.')[2]
+    local len = vim.fn.len(vim.fn.getline(l))
+
+    func(args, {  l-1, 0, l-1, len })
+end
+
+return M
--- lua/main/runBuf.lua
+++ lua/main/runBuf.lua
@@ -3,30 +3,19 @@
 local vim = vim -- fix lsp warning
 local destr = table.unpack or unpack
 
-
 local function luaFunc(str)
     local func, err = load(str)
-    if func then
-        func()
+    if func then func()
     else
-        vim.api.nvim_call_function('PrintError', { 'Cannot execute Lua chunk: ' .. err })
+        vim.api.nvim_echo({{ 'Cannot execute Lua chunk: ' .. err, 'ErrorMsg' }}, true, {})
     end
 end
+local function vimFunc(str) vim.cmd(str) end
+local function keyFunc(str) vim.fn.feedkeys(str) end
 
-local function vimFunc(str)
-    vim.cmd(str)
-end
+local function runBufferCommand(args, coords)
+    local func, delete = destr(args)
 
-local function keyFunc(str)
-    vim.fn.feedkeys(str)
-end
-
-local function printError(msg)
-    vim.cmd.redraw() -- https://stackoverflow.com/a/19206860/18704284
-    vim.api.nvim_echo({{ msg, 'ErrorMsg' }}, true, {})
-end
-
-local function runBufferCommand(func, delete, coords)
     local sl, sc, el, ec = destr(coords)
 
     local str = table.concat(vim.api.nvim_buf_get_text(0, sl, sc, el, ec, {}), '\n')
@@ -36,104 +25,19 @@ local function runBufferCommand(func, delete, coords)
     func(str)
 end
 
-local function fixMarkerCoords(sl, sc, el, ec) -- pray that this will work
-    sl = sl - 1
-    el = el - 1
-    sc = sc - 1
-    ec = ec - 1
-
-    local lastCharLen = vim.fn.len(vim.fn.strpart(vim.fn.getline(el+1), ec, 1, false))
-    ec = ec + lastCharLen
-
-    return { sl, sc, el, ec }
-end
-
---run command with text object
-local function callOpfunc(args)
-    local cursorPos = vim.fn.getpos('.')
-    _G.OpFunc = function(type)
-        vim.fn.setpos('.', cursorPos)
-
-        if type ~= 'line' and type ~= 'char' then printError('Opfunc type '..type..' is not supported'); return end
-        local sl, sc = destr(vim.fn.getpos("'["), 2, 3)
-        local el, ec = destr(vim.fn.getpos("']"), 2, 3)
-
-        local t = { destr(args) }
-        table.insert(t, fixMarkerCoords(sl, sc, el, ec))
-
-        runBufferCommand(destr(t))
-    end
-    vim.cmd[=[set opfunc=v:lua.OpFunc]=]
-    vim.fn.feedkeys('g@', 'n')
-end
-
---run command with visual selection
-local function callVisual(args)
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'nx', true)
-    local type = vim.fn.visualmode()
-    if type ~= 'v' and type ~= 'V' then printError('Visual mode '..type..' is not supported'); return end -- visual block
-
-    local sl, sc = destr(vim.fn.getpos("'<"), 2, 3)
-    local el, ec = destr(vim.fn.getpos("'>"), 2, 3)
-
-    if vim.api.nvim_get_option('selection') == 'exclusive' then
-        sl = sl - 1
-        sc = sc - 1
-        el = el - 1
-        ec = ec - 1 -- end is already exclusive
-    else
-        sl, sc, el, ec = destr(fixMarkerCoords(sl, sc, el, ec))
-    end
-
-    local t = { destr(args) }
-    table.insert(t, { sl, sc, el, ec })
-
-    runBufferCommand(destr(t))
-end
-
---run command with current line
-local function callLine(args)
-    local l = vim.fn.getpos('.')[2]
-    local len = vim.fn.len(vim.fn.getline(l))
-
-    local t = { destr(args) }
-    table.insert(t, { l-1, 0, l-1, len })
-
-    runBufferCommand(destr(t))
-end
-
-
+local selection = require('main.getSelection')
 local m = require('mapping')
 
-m.n('<leader>rl', function() callOpfunc({luaFunc, false}) end)
-m.n('<leader>rv', function() callOpfunc({vimFunc, false}) end)
-m.n('<leader>rk', function() callOpfunc({keyFunc, false}) end)
+for _, delete in pairs({ false, true }) do
+    local delSymbol
+    if delete then delSymbol = 'd' else delSymbol = '' end
+    local prefix = '<leader>r'..delSymbol
 
-m.x('<leader>rl', function() callVisual({luaFunc, false}) end)
-m.x('<leader>rv', function() callVisual({vimFunc, false}) end)
-m.x('<leader>rk', function() callVisual({keyFunc, false}) end)
-
-m.n('<leader>rll', function() callLine({luaFunc, false}) end)
-m.n('<leader>rvv', function() callLine({vimFunc, false}) end)
-m.n('<leader>rkk', function() callLine({keyFunc, false}) end)
-
-m.n('<leader>rL', function() callLine({luaFunc, false}) end)
-m.n('<leader>rV', function() callLine({vimFunc, false}) end)
-m.n('<leader>rK', function() callLine({keyFunc, false}) end)
-
-
-m.n('<leader>rdl', function() callOpfunc({luaFunc, true}) end)
-m.n('<leader>rdv', function() callOpfunc({vimFunc, true}) end)
-m.n('<leader>rdk', function() callOpfunc({keyFunc, true}) end)
-
-m.x('<leader>rdl', function() callVisual({luaFunc, true}) end)
-m.x('<leader>rdv', function() callVisual({vimFunc, true}) end)
-m.x('<leader>rdk', function() callVisual({keyFunc, true}) end)
-
-m.n('<leader>rdll', function() callLine({luaFunc, true}) end)
-m.n('<leader>rdvv', function() callLine({vimFunc, true}) end)
-m.n('<leader>rdkk', function() callLine({keyFunc, true}) end)
-
-m.n('<leader>rdL', function() callLine({luaFunc, true}) end)
-m.n('<leader>rdV', function() callLine({vimFunc, true}) end)
-m.n('<leader>rdK', function() callLine({keyFunc, true}) end)
+    for key, func in pairs({ l = luaFunc, v = vimFunc, k = keyFunc, }) do
+        local args = { func, delete }
+        m.n(prefix..key:upper(), function() selection.callLine  (runBufferCommand, args) end)
+        m.n(prefix..key..key,    function() selection.callLine  (runBufferCommand, args) end)
+        m.n(prefix..key,  function() return selection.callOpfunc(runBufferCommand, args) end, { expr = true })
+        m.x(prefix..key,         function() selection.callVisual(runBufferCommand, args) end)
+    end
+end
