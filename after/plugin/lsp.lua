b12b06e24489487b8cef2b8007c52e6363921cd2
vanaigr <vanaigranov@gmail.com>
2024-09-03 16:08:01 -0500

    move typescript-tools config to lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -80,3 +80,24 @@ conf('clang', function()
         capabilities = capabilities,
     }
 end)
+
+require("typescript-tools").setup {
+  settings = {
+    separate_diagnostic_server = false,
+    publish_diagnostic_on = "insert_leave",
+    expose_as_code_action = { "add_missing_imports" },
+    tsserver_plugins = {},
+    tsserver_max_memory = "auto",
+    tsserver_format_options = {},
+    tsserver_file_preferences = {},
+    tsserver_locale = "en",
+    complete_function_calls = false,
+    include_completions_with_insert_text = true,
+    code_lens = "off",
+    disable_member_code_lens = true,
+    jsx_close_tag = {
+        enable = true,
+        filetypes = { "javascriptreact", "typescriptreact" },
+    }
+  },
+}
