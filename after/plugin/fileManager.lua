1de6d3ff04e244b0131776e9bd97885a719a1885
vanaigr <vanaigranov@gmail.com>
2024-11-29 16:26:39 -0600

    fix: conflicting lua filetype mappings

--- after/plugin/fileManager.lua
+++ after/plugin/fileManager.lua
@@ -1,4 +1,4 @@
 local m = require('mapping')
 require('triptych').setup()
 m.n('<leader>e', '<cmd>Triptych<cr>')
-m.n('<leader><leader>e', '<cmd>tab split<cr><cmd>Triptych<cr>')
+m.n('<leader>te', '<cmd>tab split<cr><cmd>Triptych<cr>')
