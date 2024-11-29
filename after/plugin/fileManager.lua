6d9ec094525d809529d7055b75f3b98d61954ebc
vanaigr <vanaigranov@gmail.com>
2024-11-29 16:26:54 -0600

    add file manager icons

--- after/plugin/fileManager.lua
+++ after/plugin/fileManager.lua
@@ -1,4 +1,13 @@
 local m = require('mapping')
-require('triptych').setup()
+require('triptych').setup{
+    options = {
+      show_hidden = true,
+      file_icons = {
+          enabled = true,
+          directory_icon = '📁',
+          fallback_file_icon = '📄',
+      },
+    },
+}
 m.n('<leader>e', '<cmd>Triptych<cr>')
 m.n('<leader>te', '<cmd>tab split<cr><cmd>Triptych<cr>')
